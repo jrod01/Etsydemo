7988fe7dd309d6173f55738cef1fe4411b7a119b
Jonathan Rodriguez <jrguezg1@gmail.com>
2015-06-27 14:31:13 -0400

    added figaro gem and privacy settigns

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,9 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "100x", :thumb => "50x50>" }, :default_url => "default.jpg"
+  has_attached_file :image, :styles => { :medium => "100x", :thumb => "50x50>" }, :default_url => "default.jpg",
+                            :storage => :dropbox,
+                            :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+end
+
+class Listing < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
